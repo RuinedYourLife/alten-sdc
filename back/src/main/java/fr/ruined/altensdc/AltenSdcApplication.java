e981a57bff6534c01a3db5dbee83595e1e1443c4
RuinedYourLife <debhi.quentin@gmail.com>
2023-10-27 17:28:30 +0000

    init: spring

---
+++ back/src/main/java/fr/ruined/altensdc/AltenSdcApplication.java
@@ -0,0 +1,13 @@
+package fr.ruined.altensdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltenSdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltenSdcApplication.class, args);
+	}
+
+}
