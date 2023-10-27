bec40668da5596e0027aa236eafd1088230f9fce
RuinedYourLife <debhi.quentin@gmail.com>
2023-10-27 17:32:37 +0000

    feat: product model

---
+++ back/src/main/java/fr/ruined/altensdc/model/Product.java
@@ -0,0 +1,19 @@
+package fr.ruined.altensdc.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Product {
+  @Id
+  private String id;
+  private String code;
+  private String name;
+  private String description;
+  private double price;
+  private int quantity;
+  private String inventoryStatus;
+  private String category;
+  private String image;
+  private int rating;
+}
