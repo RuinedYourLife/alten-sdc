036f9c07691b97cb48979c3c4ba34759b1b69706
RuinedYourLife <debhi.quentin@gmail.com>
2023-10-27 19:43:45 +0000

    feat: backend & frontend communication

--- front/src/app/products/products.service.ts
+++ front/src/app/products/products.service.ts
@@ -1,67 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import {  Observable } from 'rxjs';
 import { Product } from './product.class';
+import { environment } from 'environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-    private static productslist: Product[] = null;
-    private products$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+    private BASE_URL = environment.apiUrl + '/product';
 
     constructor(private http: HttpClient) { }
 
     getProducts(): Observable<Product[]> {
-        if( ! ProductsService.productslist )
-        {
-            this.http.get<any>('assets/products.json').subscribe(data => {
-                ProductsService.productslist = data.data;
-                
-                this.products$.next(ProductsService.productslist);
-            });
-        }
-        else
-        {
-            this.products$.next(ProductsService.productslist);
-        }
-
-        return this.products$;
+        return this.http.get<Product[]>(this.BASE_URL);
     }
 
-    create(prod: Product): Observable<Product[]> {
-
-        ProductsService.productslist.push(prod);
-        this.products$.next(ProductsService.productslist);
-        
-        return this.products$;
+    create(prod: Product): Observable<Product> {
+        return this.http.post<Product>(this.BASE_URL, prod);
     }
 
-    update(prod: Product): Observable<Product[]>{
-        ProductsService.productslist.forEach(element => {
-            if(element.id == prod.id)
-            {
-                element.name = prod.name;
-                element.category = prod.category;
-                element.code = prod.code;
-                element.description = prod.description;
-                element.image = prod.image;
-                element.inventoryStatus = prod.inventoryStatus;
-                element.price = prod.price;
-                element.quantity = prod.quantity;
-                element.rating = prod.rating;
-            }
-        });
-        this.products$.next(ProductsService.productslist);
-
-        return this.products$;
+    update(prod: Product): Observable<Product>{
+        const updateUrl = `${this.BASE_URL}/${prod.id}`;
+        return this.http.patch<Product>(updateUrl, prod);
     }
 
-
-    delete(id: number): Observable<Product[]>{
-        ProductsService.productslist = ProductsService.productslist.filter(value => { return value.id !== id } );
-        this.products$.next(ProductsService.productslist);
-        return this.products$;
+    delete(id: number): Observable<void>{
+        const deleteUrl = `${this.BASE_URL}/${id}`;
+        return this.http.delete<void>(deleteUrl);
     }
 }
---
+++ front/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://51.38.60.115:3000/api'
+};
